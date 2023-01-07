72cbad52ff64b08d7bed229ec4887deab290a0d3
start.vaadin.com <no-reply@vaadin.com>
2023-01-07 20:00:39 +0000

    Generated project

---
+++ src/main/java/org/codemash/runnerz/ui/Application.java
@@ -0,0 +1,47 @@
+package org.codemash.runnerz.ui;
+
+import com.vaadin.flow.component.dependency.NpmPackage;
+import com.vaadin.flow.component.page.AppShellConfigurator;
+import com.vaadin.flow.theme.Theme;
+import com.vaadin.flow.theme.lumo.Lumo;
+import javax.sql.DataSource;
+import org.codemash.runnerz.ui.data.service.SamplePersonRepository;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.sql.init.SqlDataSourceScriptDatabaseInitializer;
+import org.springframework.boot.autoconfigure.sql.init.SqlInitializationProperties;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
+
+/**
+ * The entry point of the Spring Boot application.
+ *
+ * Use the @PWA annotation make the application installable on phones, tablets
+ * and some desktop browsers.
+ *
+ */
+@SpringBootApplication
+@Theme(value = "runnerz-ui", variant = Lumo.DARK)
+@NpmPackage(value = "line-awesome", version = "1.3.0")
+@NpmPackage(value = "@vaadin-component-factory/vcf-nav", version = "1.0.6")
+public class Application extends SpringBootServletInitializer implements AppShellConfigurator {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+    @Bean
+    SqlDataSourceScriptDatabaseInitializer dataSourceScriptDatabaseInitializer(DataSource dataSource,
+            SqlInitializationProperties properties, SamplePersonRepository repository) {
+        // This bean ensures the database is only initialized when empty
+        return new SqlDataSourceScriptDatabaseInitializer(dataSource, properties) {
+            @Override
+            public boolean initializeDatabase() {
+                if (repository.count() == 0L) {
+                    return super.initializeDatabase();
+                }
+                return false;
+            }
+        };
+    }
+}
